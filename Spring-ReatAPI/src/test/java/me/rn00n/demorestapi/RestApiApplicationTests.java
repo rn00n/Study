69c91e1bad34895563bf7859167d84be6a6fcc90
rn00n <rn00n@naver.com>
2020-06-23 16:52:31 +0900

    add spring-restapi project

---
+++ Spring-ReatAPI/src/test/java/me/rn00n/demorestapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rn00n.demorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
