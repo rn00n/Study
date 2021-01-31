657d16e2951c26fa7ba29b0beef649c943de313b
rn00n <rn00n@naver.com>
2021-01-31 20:43:54 +0900

    기본 시큐리티 설정

--- JWT/src/main/java/me/rn00n/jwt/MainController.java
+++ JWT/src/main/java/me/rn00n/jwt/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MainController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
--- JWT/src/main/java/me/rn00n/jwt/infra/config/SecurityConfig.java
+++ JWT/src/main/java/me/rn00n/jwt/infra/config/SecurityConfig.java
@@ -1,4 +1,17 @@
 package me.rn00n.jwt.infra.config;
 
-public class SecurityConfig {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() // http servlet request 접근제한 설정
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated()
+        ;
+    }
 }
