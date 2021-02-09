8340856f3932c9f2f8297a8f7306d8914ff1278a
rn00n <rn00n@naver.com>
2021-02-09 16:47:52 +0900

    commit

--- OAuth_JWT_Account_Auth_Resource_Restdocs/src/test/java/me/rn00n/server/infra/commons/BaseControllerTest.java
+++ OAuth_JWT_Account_Auth_Resource_Restdocs/src/test/java/me/rn00n/server/infra/commons/BaseControllerTest.java
@@ -2,10 +2,12 @@ package me.rn00n.server.infra.commons;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.ActiveProfiles;
 
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles(TestProfile.TEST)
+@ExtendWith({RestDocumentationExtension.class}) // Junit5 에서의 resrdocs 설정
 @Disabled
 public class BaseControllerTest {
 
