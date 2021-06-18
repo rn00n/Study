cf51eed75fba1635ded40b1097e2a204ffe205e3
rn00n <rn00n@naver.com>
2021-06-19 00:26:18 +0900

    commit

---
+++ NFT_MarketPlace/src/test/java/ko/co/nftmarketplace/nft/NftApplicationTests.java
@@ -0,0 +1,13 @@
+package ko.co.nftmarketplace.nft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
